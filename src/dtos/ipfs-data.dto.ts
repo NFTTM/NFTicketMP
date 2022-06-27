02c2c9d1e6b72747964aea554e292788e0b2f904
caixiangfan <--help>
2022-06-27 22:35:51 +0000

    updated ipfs dto and share the service

--- src/dtos/ipfs-data.dto.ts
+++ src/dtos/ipfs-data.dto.ts
@@ -1,3 +1,8 @@
 export class IpfsDataDto {
-  constructor(public path: string, public cid: object, public size: number) {}
+  constructor(
+    public IpfsHash: string,
+    public PinSize: number,
+    public Timestamp: string,
+    public isDuplicate?: boolean,
+  ) { }
 }
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { ProviderService } from './services/provider/provider.service';
 import { WalletService } from './services/wallet/wallet.service';
 import { SignerService } from './services/signer/signer.service';
+import { IpfsService } from './services/ipfs/ipfs.service';
 
 @Global()
 @Module({
-  providers: [ProviderService, WalletService, SignerService],
-  exports: [ProviderService, WalletService, SignerService],
+  providers: [ProviderService, WalletService, SignerService, IpfsService],
+  exports: [ProviderService, WalletService, SignerService, IpfsService],
 })
-export class SharedModule {}
+export class SharedModule { }
